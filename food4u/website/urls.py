73400c09c3f8524c9f8f3395e4e60ee331ed1c36
Maksym Lan <lanmaksym@gmail.com>
2025-02-21 17:24:53 -0600

    Registration added

--- food4u/website/urls.py
+++ food4u/website/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include
+from registration.views import CustomLoginView
 from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('accounts/', include('registration.urls')),
+    path('login/', CustomLoginView.as_view(), name='member_login'),
+    path('logout/', views.user_logout_view, name='member_logout'),
 ]
--- food4u/website/views.py
+++ food4u/website/views.py
@@ -1,3 +1,13 @@
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from django.contrib.auth import logout
+from django.views.decorators.cache import never_cache
 from django.shortcuts import render
 def home_view(request):
     return render(request, 'website/home.html')
+@never_cache
+@require_POST
+def user_logout_view(request):
+    logout(request)
+    response = redirect('home')
+    return response
