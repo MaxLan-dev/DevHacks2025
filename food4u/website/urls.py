692009c698446e97d1da60d27659becfc64d11da
drmydrmy <vms05new@ya.ru>
2025-02-21 17:28:25 -0600

    Added views

--- food4u/website/urls.py
+++ food4u/website/urls.py
@@ -4,4 +4,8 @@ from django.conf.urls.static import static
 from . import views
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('profile/<int:supplier_id>', views.profile_view, name='profile'),
+    path('search', views.search_view, name='search'),
+    path('about', views.about_view, name='about'),
+    path('search_results', views.search_results_request, name='search_results'),
 ]
--- food4u/website/views.py
+++ food4u/website/views.py
@@ -1,3 +1,95 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from db import SessionLocal, User, Supplier, Products, Review
+from sqlalchemy import select
+
 def home_view(request):
     return render(request, 'website/home.html')
+
+def about_view(request):
+    return render(request, 'website/about.html')
+
+def profile_view(request, supplier_id):
+    session = SessionLocal()
+    try:
+        query = select(Supplier).where(Supplier.id == supplier_id)
+        results = session.scalar(query)
+        supplier_dict = {'name' : results.name, 
+                        'address' : results.address, 
+                        'email' : results.email, 
+                        'phone' : results.phone, 
+                        'industry' : results.industry, 
+                        'date_registered' : results.date_registered, 
+                        'description' : results.description}
+        return render(request, 'website/profile.html', supplier_dict)
+    except Exception as e:
+        session.rollback()
+        return render(request, 'website/error.html', {'error': str(e)})
+    finally:
+        session.close()
+
+def search_view(request):
+    session = SessionLocal()
+    try:
+        query = select(Supplier)
+        results = session.scalars(query)
+        suppliers = []
+        for result in results:
+            supplier_dict = {'name' : result.name, 
+                            'address' : result.address, 
+                            'email' : result.email, 
+                            'phone' : result.phone, 
+                            'industry' : result.industry, 
+                            'date_registered' : result.date_registered, 
+                            'description' : result.description}
+            suppliers.append(supplier_dict)
+        return render(request, 'website/profile.html', {'suppliers' : suppliers, 'is_authenticated' : request.user.is_authenticated})
+    except Exception as e:
+        session.rollback()
+        return render(request, 'website/error.html', {'error': str(e)})
+    finally:
+        session.close()
+    return render(request, 'website/search.html')
+
+def search_results_request(request):
+    # Industry, Location, Rating
+    industry = request.GET.get('industry', 'any')
+    #location = request.GET.get('location', '1000')
+    rating_min = request.GET.get('rating_min', '1')
+    rating_max = request.GET.get('rating_max', '5')
+    sort_by = request.GET.get('sort_by', 'name')
+    hide_no_rating = request.GET.get('no_rating', 'false')
+    if sort_by == 'name':
+        sort_by = Supplier.name
+    #elif sort_by == 'rating':
+    session = SessionLocal()
+    try:
+        query = select(Supplier)
+        if industry != 'any':
+            query = query.where(Supplier.industry == industry)
+        results = session.scalars(query)
+        for supplier in results:
+            rating_sum = 0
+            if supplier.reviews.length == 0 and hide_no_rating == 'true':
+                continue
+            for review in supplier.reviews:
+                rating_sum += review.rating
+            if rating_sum / supplier.reviews.length < rating_min or rating_sum / supplier.reviews.length > rating_max:
+                continue
+
+        suppliers = []
+        for result in results:
+            supplier_dict = {'name' : result.name, 
+                            'address' : result.address, 
+                            'email' : result.email, 
+                            'phone' : result.phone, 
+                            'industry' : result.industry, 
+                            'date_registered' : result.date_registered, 
+                            'description' : result.description}
+            suppliers.append(supplier_dict)
+            return JsonResponse({"message": "Hello, world!"})
+    except Exception as e:
+        session.rollback()
+        return render(request, 'website/error.html', {'error': str(e)})
+    finally:
+        session.close()
