e133645557416f4e36daad44f764089601ea8b43
MassonMassoniy <118527160+MassonMassoniy@users.noreply.github.com>
2025-02-22 11:21:34 -0600

    333

--- food4u/website/views.py
+++ food4u/website/views.py
@@ -7,6 +7,8 @@ from django.shortcuts import render
 from .db import SessionLocal, User, Supplier, Products, Review
 from sqlalchemy import select
 
+from datetime import datetime
+
 def home_view(request):
     return render(request, 'website/home.html')
 def wishlist_view(request):
@@ -18,14 +20,18 @@ def profile_view(request, supplier_id):
     try:
         query = select(Supplier).where(Supplier.id == supplier_id)
         results = session.scalar(query)
-        supplier_dict = {'name' : results.name, 
-                        'address' : results.address, 
-                        'email' : results.email, 
-                        'phone' : results.phone, 
-                        'industry' : results.industry, 
-                        'date_registered' : results.date_registered, 
+        print(results.reviews)
+        supplier_dict = {
+                        'id' : results.id,
+                        'name' : results.name,
+                        'address' : results.address,
+                        'email' : results.email,
+                        'phone' : results.phone,
+                        'industry' : results.industry,
+                        'date_registered' : results.date_registered,
                         'description' : results.description,
                         'reviews' : results.reviews}
+        print(supplier_dict)
         return render(request, 'website/profile.html', supplier_dict)
     except Exception as e:
         session.rollback()
@@ -126,3 +132,25 @@ def user_logout_view(request):
     logout(request)
     response = redirect('home')
     return response
+
+
+
+
+session = SessionLocal()
+try:
+    rev1 = Review(writer_id=1, product_id=1, supplier_id=2, rating=5, label="test review1", content="test content1", date=datetime.now())
+    rev2 = Review(writer_id=1, product_id=1, supplier_id=2, rating=4, label="test review2", content="test content2", date=datetime.now())
+    #test_supplier = Supplier(name="test supplsier2", address="test address2", email="tests email2", phone="test phone2", industry="test industry2", description="test description2",
+                             #reviews=[rev1, rev2])
+    session.add(rev1)
+    session.add(rev2)
+    #session.add(test_supplier)
+    session.commit()
+    print("adsadasdassa")
+    #data = [obj.to_dict() for obj in results]  # Assume your models have a to_dict method
+except Exception as e:
+    print(e)
+    print("errorerrorerrorerror")
+    session.rollback()
+finally:
+    session.close()
