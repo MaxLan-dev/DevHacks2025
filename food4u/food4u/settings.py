1cb4b5e0455877553927c597d432b3de6a403500
EvgenProDev <151104243+EvgenProDev@users.noreply.github.com>
2025-02-23 18:49:20 -0600

    Updated settings.py, add 'food4u-5ka6.onrender.com' to ALLOWED_HOSTS

--- food4u/food4u/settings.py
+++ food4u/food4u/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'food4u-5ka6.onrender.com']
 AUTH_USER_MODEL = 'registration.CustomUser'
 
 
